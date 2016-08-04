a0c037ff114655982fc73731e0e8defe89624436
김은영 <rindabia@gmail.com>
2016-08-04 14:53:03 +0900

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Kim Eunyoung on 2016. 8. 4..
+//  Copyright © 2016년 Kim Eunyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
